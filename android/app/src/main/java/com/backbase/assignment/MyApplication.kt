d4a0948c906398d55c94cede45d24820d9138fda
Mohamed Hashim <mohamedhashim28@gmail.com>
2021-11-15 13:02:19 +0100

    Add navigation component dependencies

---
+++ android/app/src/main/java/com/backbase/assignment/MyApplication.kt
@@ -0,0 +1,11 @@
+package com.backbase.assignment
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * Created by Mohamed Hashim on 15/11/2021.
+ */
+
+@HiltAndroidApp
+class MyApplication : Application()

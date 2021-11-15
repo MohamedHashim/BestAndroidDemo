856e6d5efc262ad169d71122c013cc22f853bd0e
Mohamed Hashim <mohamedhashim28@gmail.com>
2021-11-15 13:12:58 +0100

    Add navigation navGraph and host it in main activity

---
+++ android/app/src/main/java/com/backbase/assignment/features/movies/presentation/MoviesFragment.kt
@@ -0,0 +1,23 @@
+package com.backbase.assignment.features.movies.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.backbase.assignment.R
+
+/**
+ * Created by Mohamed Hashim on 15/11/2021.
+ */
+class MoviesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        return inflater.inflate(R.layout.fragment_movies, container, false)
+    }
+}
